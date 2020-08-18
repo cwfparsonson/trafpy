360e96af4245056f2c42f9ed93ec97b949b66d15
Christopher Parsonson <cwfparsonson@gmail.com>
2020-08-18 15:43:41 +0100

    imported trafpy in conf.py

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -63,6 +63,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+import trafpy
 
 # -- Options for HTML output -------------------------------------------------
 
