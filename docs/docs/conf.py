c2247d06c4e1d84301553fbbfc0a14bd44292f12
Christopher Parsonson <cwfparsonson@gmail.com>
2020-08-18 15:58:04 +0100

    added .readthedocs.yml config file

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -63,8 +63,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-import trafpy
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
