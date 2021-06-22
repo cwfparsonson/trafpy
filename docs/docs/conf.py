b9a555072210580d39a0b50b3e1b26cde0749a1c
Christopher Parsonson <cwfparsonson@gmail.com>
2021-06-22 11:17:11 +0100

    :bug: Commented tutorial to help debug readthedocs.

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -90,4 +90,5 @@ github_url = 'https://github.com/cwfparsonson/trafpy'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+html_static_path = [] # don't have _static file atm so removing this to remove warnings
